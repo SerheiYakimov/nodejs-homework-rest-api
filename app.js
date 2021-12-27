e72cacd7be39136072f3349528242786f0116e49
SerheiYakimov <ysergey2901@gmail.com>
2021-12-27 23:47:24 +0200

    connect mongoose

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import { HttpCode } from './lib/constants';
 
-import contactsRouter from './routes/api/controllers/contacts/contacts';
+import contactsRouter from './routes/api/contacts';
 
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
 
 app.use((_req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res
+    .status(HttpCode.NOT_FOUND)
+    .json({
+      status: 'error',
+      code: HttpCode.NOT_FOUND,
+      message: 'Not found',
+    })
 })
 
 app.use((err, _req, res, _next) => {
-  res.status(500).json({ message: err.message })
+  res
+    .status(HttpCode.INTERNAL_SERVER_ERROR)
+    .json({
+      status: 'fail',
+      code: HttpCode.INTERNAL_SERVER_ERROR,
+      message: err.message,
+    })
 })
 
 export default app;
---
+++ midllewares/validations/contactsValidation.js
@@ -0,0 +1,106 @@
+/* eslint-disable prefer-regex-literals */
+/* eslint-disable no-invalid-regexp */
+import Joi from 'joi';
+import pkg from 'mongoose';
+import { MIN_AGE, MAX_AGE } from '../../lib/constants';
+
+
+const { Types } = pkg;
+
+const createSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(10).max(20).required(),
+    age: Joi.number().integer().min(MIN_AGE).max(MAX_AGE).optional(),
+    favorite: Joi.bool().optional(),
+})
+
+const updateSchema = Joi.object({
+    name: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().optional(),
+    age: Joi.number().integer().min(MIN_AGE).max(MAX_AGE).optional(),
+    favorite: Joi.bool().optional(),
+}).or('name', 'email', 'phone', 'age')
+
+const updateFavoriteSchema = Joi.object({
+    favorite: Joi.bool().required(),
+})
+
+const querySchema = Joi.object({
+    limit: Joi.string().pattern(new RegExp('\\d+')).optional(),
+    skip: Joi.number().min(0).optional(),
+    sortBy: Joi.string().valid('name', 'email', 'age', 'phone', 'favorite').optional(),
+    sortByDesc: Joi.string().valid('name', 'email', 'age', 'phone', 'favorite').optional(),
+    filter: Joi
+        .string()
+        .pattern(new RegExp('(name|email|age|phone|favorite)\\|?(name|email|age|phone|favorite)+'))
+        .optional()
+})
+
+export const validateCreate = async (req, res, next) => {
+    try {
+        await createSchema.validateAsync(req.body);
+    }
+    catch (err) {
+        const [{ type }] = err.details;
+        if (type === 'string.email') {
+            return res.status(400).json({ message: err.message.replace(/"/g, '') })
+        }
+        if (type === 'object.unknown') {
+            return res.status(400).json({ message: err.message.replace(/"/g, '') })
+        }
+        return res.status(400).json({ message: 'missing required name field' })
+    }
+    next()
+}
+
+export const validateUpdate = async (req, res, next) => {
+    try {
+        await updateSchema.validateAsync(req.body);
+    }
+    catch (err) {
+        const [{ type }] = err.details;
+        if (type === 'string.email') {
+            return res.status(400).json({ message: err.message.replace(/"/g, '') })
+        }
+        if (type === 'object.unknown') {
+            return res.status(400).json({ message: err.message.replace(/"/g, '') })
+        }
+        return res.status(400).json({ message: 'Missing fields' })
+    }
+    next()
+}
+
+export const validateUpdateFavorite = async (req, res, next) => {
+    try {
+        await updateFavoriteSchema.validateAsync(req.body);
+    }
+    catch (err) {
+        const [{ type }] = err.details;
+        if (type === 'object.missing') {
+           return res.status(400).json({ message: 'Missing fields favorite' })
+        }
+        return res.status(400).json({ message: err.message })
+    }
+    next()
+}
+
+export const validateId = async (req, res, next) => {
+    if (!Types.ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({ message: 'Invalid OdjectId' })
+    }
+    next()
+}
+
+export const validateQuery = async (req, res, next) => {
+    try {
+        await querySchema.validateAsync(req.query);
+    }
+    catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'missing required name field' })
+    }
+    next()
+}
